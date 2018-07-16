81968975cc014d10cfa08b0a6fd5923fc448c838
agufranov <al.gufranov@gmail.com>
2018-07-16 04:30:57 +0300

    Reselect & main data selector

--- client/src/components/main/connector.ts
+++ client/src/components/main/connector.ts
@@ -5,16 +5,19 @@ import R from 'ramda'
 import { connect } from 'react-redux'
 import { ThunkDispatch } from 'redux-thunk'
 
+import { createSelector } from 'reselect'
 import { StoreState } from '../../redux/StoreState'
 
 import MainComponent, { DispatchProps, StateProps } from '.'
 import { categoriesFetch, mainFetch, placesFetch } from '../../redux/fetchers'
 import { groupOps, toOperationExt } from '../../util'
 
-export default connect(
-    (state: StoreState): StateProps => {
+const rawOpsSelector = (state: StoreState) => state.main.data
+const dataSelector = createSelector(
+    rawOpsSelector,
+    (rawOps) => {
         const now = moment()
-        const data = chain(state.main.data)
+        const data = chain(rawOps)
             .map(toOperationExt)
             .orderBy('datetime', 'desc')
             .takeWhile(d => now.diff(d.datetime.clone(), 'days') < 5)
@@ -25,6 +28,10 @@ export default connect(
             dataByDay: groupOps('day', data),
         }
     },
+)
+
+export default connect(
+    dataSelector,
     // tslint:disable-next-line:no-any
     (dispatch: ThunkDispatch<StoreState, void, any>): DispatchProps => ({
         // tslint:disable-next-line:arrow-return-shorthand
